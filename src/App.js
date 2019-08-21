b20250f320e768196f5b445cc374d56ebf87535f
“Patrick <pctennislessons@gmail.com>
2019-08-21 06:50:56 -0600

    finished first major form

--- src/App.js
+++ src/App.js
@@ -5,20 +5,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Albert's Potty tracker</h1>
+        <button>Potty!</button>
+        <button>Not.</button>
+        <form id="record-potty">
+            <label>Number:</label>
+            <input type="number" name="email" placeholder="1 2"/>
+            <label>Notes</label>
+            <input type="text" name="first_name" placeholder="Notes?"/>
+            <label>Date</label>
+            <input type="date" name="birth_date"/>
+            <p>
+                <label>Where</label>
+                <input type="radio" name="where" value="Carpet"/>Carpet
+                <input type="radio" name="where" value="Hardwood"/>Hardwood
+                <input type="radio" name="where" value="Concret"/>Concret
+                <input type="radio" name="where" value="Grass"/>Grass
+                <input type="radio" name="where" value="Potty"/>Potty
+            </p>
+            <input type="submit" value="SUBMIT"/>
+       </form>
+
+
     </div>
   );
 }
