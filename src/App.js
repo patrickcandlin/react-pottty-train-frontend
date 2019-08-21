c420d7bbf42869ff900340b23ca3e5875b7bef7c
“Patrick <pctennislessons@gmail.com>
2019-08-21 10:32:23 -0600

    post request is now working from frontend

--- src/App.js
+++ src/App.js
@@ -2,7 +2,30 @@ import React, { Component } from "react";
 
 import './App.css';
 
+const pottyUrl = "https://albert-potty-train-backend.herokuapp.com/potties"
+
 export default class App extends Component {
+  state={
+    number: "",
+    notes: "",
+    date: "",
+    where: "",
+  }
+
+  submitPotty = (event) => {
+
+    event.preventDefault()
+    console.log('save this potty')
+    fetch(pottyUrl, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                number: 1,
+                notes: 'he was focusing on his blocks',
+                date: '2019/08/21',
+                where: 'carpet',
+            })})
+  }
   
   render() {
     return (
@@ -10,7 +33,7 @@ export default class App extends Component {
         <h1>Albert's Potty tracker</h1>
           <button>Potty!</button>
           <button>Not.</button>
-          <form id="record-potty">
+          <form id="record-potty" onSubmit={this.submitPotty}>
               <label>Number:</label>
               <input type="number" name="email" placeholder="1 2"/>
               <label>Notes</label>
@@ -21,8 +44,7 @@ export default class App extends Component {
                   <label>Where</label>
                   <input type="radio" name="where" value="Carpet"/>Carpet
                   <input type="radio" name="where" value="Hardwood"/>Hardwood
-                  <input type="radio" name="where" value="Concret"/>Concret
-                  <input type="radio" name="where" value="Grass"/>Grass
+                  <input type="radio" name="where" value="Outside"/>Outside
                   <input type="radio" name="where" value="Potty"/>Potty
               </p>
               <input type="submit" value="SUBMIT"/>
