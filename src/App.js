dde05bf50ca4c039ec570ad26ed0251a550abcad
“Patrick <pctennislessons@gmail.com>
2019-08-21 15:58:04 -0600

    delete is now working

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import PottyForm from "./components/PottyForm"
 import PottiesContainer from "./components/PottiesContainer"
 import './App.css';
 
-const pottyUrl = "https://albert-potty-train-backend.herokuapp.com/potties"
-const localUrl = "http://localhost:3000/potties"
+// const pottyUrl = "https://albert-potty-train-backend.herokuapp.com/potties"
+const pottyUrl = "http://localhost:3000/potties"
 
 export default class App extends Component {
  
@@ -18,6 +18,16 @@ export default class App extends Component {
       .then(json => this.setState(
         {potties: json}
       ))
+  }
+
+  deletePotty = (pottyId) => {
+      fetch(pottyUrl+`/${pottyId}`, {
+      method: 'DELETE',
+      headers: {'Content-Type': 'application/json'},
+     })
+     this.setState({
+        potties: this.state.potties.filter(potty => potty.id !== pottyId)
+     }) 
   }
 
   addPotty = (potty) => {
@@ -37,7 +47,9 @@ export default class App extends Component {
         <PottyForm 
           addPotty={this.addPotty}
         />
-        <PottiesContainer potties={potties}/>
+        <PottiesContainer 
+          deletePotty={this.deletePotty}
+          potties={potties}/>
       </div>
     );
   }
